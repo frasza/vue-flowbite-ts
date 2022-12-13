1f237f490dcbfaa1c7078570cbd670e54a23e1a0
frasza <zan.fraas@gmail.com>
2022-12-13 21:26:07 +0100

    Basic setup

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,8 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
+  <div class="p-2">
+    <RouterLink to="/">Home</RouterLink>
+  </div>
   <RouterView />
 </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="p-8">
+    <ul class="block">
+      <li>
+        <RouterLink
+          to="/accordion"
+          class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >Acordion</RouterLink
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
